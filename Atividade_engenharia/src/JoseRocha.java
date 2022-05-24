65f87f86856769b89371e8f973a4078b194f6184
Neto Rocha <63526723+NetoRocha97@users.noreply.github.com>
2022-05-24 16:16:49 -0300

    Implementando codigo

--- Atividade_engenharia/src/JoseRocha.java
+++ Atividade_engenharia/src/JoseRocha.java
@@ -1,4 +1,33 @@
 public class JoseRocha {
+    private int numA;
+    private int numB;
+    private int numC;
 
-    
+    public void soma() {
+        numC = numA + numB;
+    }
+
+    public int getNumA() {
+        return numA;
+    }
+
+    public void setNumA(int a) {
+        this.numA = numA;
+    }
+
+    public int getNumB() {
+        return numB;
+    }
+
+    public void setNumB(int b) {
+        this.numB = numB;
+    }
+
+    public int getNumC() {
+        return numC;
+    }
+
+    public void print() {
+        System.out.println("José Rocha do Amaral Neto");
+    }
 }
