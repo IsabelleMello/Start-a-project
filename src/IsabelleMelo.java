d2cf23b1ca087d79ea64926faa6893fd7af6d740
IsabelleMello <sisabelly301@gmail.com>
2022-05-24 16:13:19 -0300

    Método somaNumeros e printNome

---
+++ src/IsabelleMelo.java
@@ -0,0 +1,34 @@
+public class IsabelleMelo {
+
+    private int numero1;
+    private int numero2;
+    private int numero3;
+
+    public void SomaNumeros () {
+        numero3 = numero1 + numero2;
+    }
+
+    public int getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(int numero1) {
+        this.numero1 = numero1;
+    }
+
+    public int getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(int numero2) {
+        this.numero2 = numero2;
+    }
+
+    public int getNumero3() {
+        return numero3;
+    }
+    
+    public void printNome() {
+        System.out.print("Isabelle Melo do Nascimento");
+    }
+}
